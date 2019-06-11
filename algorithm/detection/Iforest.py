f53fa0eb51ddcbaa022c91c821b27d5c4178835a
summer <34535687+summerchenjuan@users.noreply.github.com>
2019-06-11 21:55:55 +0800

    iforest

---
+++ algorithm/detection/Iforest.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import IsolationForest
+from datetime import datetime
+from os import listdir
+import time
+
+class Iforest:
+    def __init__(self,df,nodelists,metrics):
+        self.df = df
+        self.nodelists = nodelists
+        self.metrics = metrics
