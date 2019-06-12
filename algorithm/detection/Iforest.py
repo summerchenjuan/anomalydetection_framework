53de507735d57ce7f8551e65de2344fa120dc896
summer <34535687+summerchenjuan@users.noreply.github.com>
2019-06-12 08:49:43 +0800

    Update Iforest.py

--- algorithm/detection/Iforest.py
+++ algorithm/detection/Iforest.py
@@ -11,3 +11,17 @@ class Iforest:
         self.nodelists = nodelists
         self.metrics = metrics
 
+    def create_train_data(self):
+        data_df = self.df[self.metrics]
+        return data_df.values
+
+    def train(self,Train_data):
+        """
+        训练模型
+        :param
+        Train_data:array(n_samples,n_features)训练数据
+        :return:model
+        """
+        model = IsolationForest(n_estimators=100, max_samples=256, contamination=0.01, behaviour='new',verbose=1)
+        model.fit(Train_data)
+        return model
